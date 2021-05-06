4585ffe8e2775f93e4d05396e5b84bba87954f7d
Carletta Walls <carlettawalls88@gmail.com>
2021-05-05 22:22:25 -0400

    updated more html, css, and js script

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,8 @@
 var citySearchEl = document.querySelector("#citySearch");
+var searchInputVal = document.querySelector("#search-input");
+var recentSearchEl = document.querySelector(".recentSearches");
+var currentResultsEl = document.querySelector(".currentResults");
+var
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
@@ -12,10 +16,16 @@ function handleSearchFormSubmit(event) {
 
 }citySearchEl.addEventListener("submit", handleSearchFormSubmit);
 
+function gatherParams() {
+    var Para
+}
+
 // add a function that allows the user to input a city name in the search box
 // function to gather the parameters
 // after inputing the user name the user can click the search button and be presented with the results for that city
 // the user is also presented with the five day results for that city
 // the city is stored under the search button along with any other recent searches
 // my personal API Key 9e25d23bcd91708b2baf5fa31ef4f1b2
-// the weather API https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} - put your personal API in the spot that says appid
+// the weather API https://api.openweathermap.org/data/2.5/forecast/daily?q + q=CITY&cnt=5units=imperial&appid=9e25d23bcd91708b2baf5fa31ef4f1b2
+
+// example of working API http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=9e25d23bcd91708b2baf5fa31ef4f1b2
